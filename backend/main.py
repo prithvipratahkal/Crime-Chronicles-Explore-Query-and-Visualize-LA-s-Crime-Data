f4ee5267764fea6205ac184acb9439d794dac94d
Akash Vegada <76861854+akashvegada@users.noreply.github.com>
2024-12-05 16:13:54 -0800

    Akash2 (#10)
    * Added post request for login, signup and querying.
    
    * login is working
    
    ---------
    
    Co-authored-by: prithviPratahkal <prithviprathakal@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,19 @@ from crime_search_engine import CrimeSearchEngine
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from passlib.context import CryptContext
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Initialize the search engine
 search_engine = CrimeSearchEngine(
